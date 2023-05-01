df33a49809cef1aed7afa65d5326d1501dd1ddc2
Joao Ferreira <jbrazf.01@gmail.com>
2023-05-01 19:53:16 +0100

    This prints to serial

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,16 @@ use standalone_rp::hal::pac;
 // higher-level drivers.
 use standalone_rp::hal;
 
+// USB Device support
+use usb_device::{class_prelude::*, prelude::*};
+
+// USB Communications Class Device support
+use usbd_serial::SerialPort;
+
+// Used to demonstrate writing formatted strings
+use core::fmt::Write;
+use heapless::String;
+
 /// Entry point to our bare-metal application.
 ///
 /// The `#[entry]` macro ensures the Cortex-M start-up code calls this function
@@ -40,6 +50,7 @@ use standalone_rp::hal;
 /// infinite loop.
 #[entry]
 fn main() -> ! {
+    // ! Setup 
     // Grab our singleton objects
     let mut pac = pac::Peripherals::take().unwrap();
     let core = pac::CorePeripherals::take().unwrap();
@@ -62,10 +73,6 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    // The delay object lets us wait for specified amounts of time (in
-    // milliseconds)
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-
     // The single-cycle I/O block controls our GPIO pins
     let sio = hal::Sio::new(pac.SIO);
 
@@ -76,21 +83,83 @@ fn main() -> ! {
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
+    
+    #[cfg(feature = "rp2040-e5")]
+    {
+        let sio = hal::Sio::new(pac.SIO);
+        let _pins = rp_picof::Pins::new(
+            pac.IO_BANK0,
+            pac.PADS_BANK0,
+            sio.gpio_bank0,
+            &mut pac.RESETS,
+        );
+    }
 
-    // Set the LED to be an output
-    let mut led_pin = pins.gpio3.into_push_pull_output();
+    // Set up the USB driver
+    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+        pac.USBCTRL_REGS,
+        pac.USBCTRL_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
 
-    let mut led_pin2 = pins.gpio0.into_push_pull_output();
+    let mut serial = SerialPort::new(&usb_bus);
 
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .manufacturer("Fake company")
+        .product("Serial port")
+        .serial_number("TEST")
+        .device_class(2) // from: https://www.usb.org/defined-class-codes
+        .build();
+
+    let mut led_pin = pins.gpio0.into_push_pull_output();
+
+    let timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS);
+
+    let delay = (clocks.system_clock.freq().to_Hz() * 2) as u64;
+
+    let mut lap_start = timer.get_counter().ticks();
+    let mut active = false;
+    let mut i = 0;
     // Blink the LED at 1 Hz
     loop {
-        led_pin2.set_high().unwrap();
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        led_pin2.set_low().unwrap();
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+        let current_tick = timer.get_counter().ticks();
+
+        let _ = serial.write(b"Hello World!\r\n");
+        
+        write_to_serial(&mut serial, current_tick);
+
+        // without this writing does not work...
+        if usb_dev.poll(&mut [&mut serial]) {
+            let mut buf = [0u8; 64];
+            let _ = serial.read(&mut buf);
+        }
     }
 }
 
+fn write_to_serial(serial_con: &mut SerialPort<hal::usb::UsbBus>, i: u64) {
+    let mut text: String<64> = String::new();
+    let _ = writeln!(&mut text, "Write to serial: {}", i);
+    let _ = serial_con.write(text.as_bytes());
+}
+
 // End of file
+/*
+writeln!(&mut text, "Current timer ticks: {}", timer.get_counter().ticks()).unwrap();
+        let _ = serial.write(text.as_bytes());
+
+        if timer.get_counter().ticks() > lap_start + delay {
+            lap_start = timer.get_counter().ticks();
+
+            writeln!(&mut text, "Current timer ticks: {}", i).unwrap();
+            let _ = serial.write(text.as_bytes());
+
+            match active {
+                true => led_pin.set_low().unwrap(),
+                false => led_pin.set_high().unwrap(),
+            }
+            active = !active;
+            i += 1;
+        }
+ */
